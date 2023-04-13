66000aae8eca9add5a3b0f097a5e71a72e39fcba
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2023-04-13 10:37:20 -0700

    chore: Add test template

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # avia-js-code-interview
-avia-js-code-interview
+Code Interview for Avia JS
+
+## Overview
+The following tasks are meant to be completed in a reasonable amount of time. The goal is to see how you approach a problem and how you structure your code. We are not looking for a perfect solution, but rather a solution that is well thought out and well structured. We are also not looking for a complete solution, but rather a solution that is complete enough to demonstrate your skills. Please feel free to ask any questions you may have.
+
+1. Write a utility function that loads the following HLS manifest and parses it into a list of objects describing each stream rendition:
+    - https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8
+    
+1. Using react, render each item in a HTML list in ascending order of bandwidth. Display the following properties for each item:
+    - the name
+    - resolution
+    - bandwidth
+    - codecs
+
+## Structure
+This project was bootstrapped with [Create React App](http://create-react-app.dev/). You can run the app in the development mode by running the `npm start` command. This will start a local server on port 3000. The page will reload if you make edits. You will also see any lint errors in the console.
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
---
+++ src/App.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+function expectText(text: string) {
+  const result = screen.getByText(text);
+  expect(result).toBeInTheDocument();
+}
+
+test('renders the manifest list', () => {
+  /**
+    #EXTM3U
+    #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2149280,CODECS="mp4a.40.2,avc1.64001f",RESOLUTION=1280x720,NAME="720"
+    url_0/193039199_mp4_h264_aac_hd_7.m3u8
+    #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=246440,CODECS="mp4a.40.5,avc1.42000d",RESOLUTION=320x184,NAME="240"
+    url_2/193039199_mp4_h264_aac_ld_7.m3u8
+    #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=460560,CODECS="mp4a.40.5,avc1.420016",RESOLUTION=512x288,NAME="380"
+    url_4/193039199_mp4_h264_aac_7.m3u8
+    #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=836280,CODECS="mp4a.40.2,avc1.64001f",RESOLUTION=848x480,NAME="480"
+    url_6/193039199_mp4_h264_aac_hq_7.m3u8
+    #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=6221600,CODECS="mp4a.40.2,avc1.640028",RESOLUTION=1920x1080,NAME="1080"
+    url_8/193039199_mp4_h264_aac_fhd_7.m3u8
+   */
+  render(<App />);
+
+  // Name
+  expectText('720');
+  expectText('240');
+  expectText('380');
+  expectText('480');
+  expectText('1080');
+  
+  // Bandwidth
+  expectText('2149280');
+  expectText('246440');
+  expectText('460560');
+  expectText('836280');
+  expectText('6221600');
+
+  // Codec
+  expectText('mp4a.40.2,avc1.64001f');
+  expectText('mp4a.40.5,avc1.42000d');
+  expectText('mp4a.40.5,avc1.420016');
+  expectText('mp4a.40.2,avc1.640028');
+
+  // Resolution
+  expectText('1280');
+  expectText('320');
+  expectText('512');
+  expectText('848');
+  expectText('1920');
+});
